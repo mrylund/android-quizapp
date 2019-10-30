297a71673fde5021780b9393389b17de31e0305f
mrylund <JEirizjiwyhyu5I>
2019-10-30 20:00:19 +0100

    Tilføjet actionbar til profile og settings

--- app/src/main/java/com/example/quiz_selvtest/ProfileAct.java
+++ app/src/main/java/com/example/quiz_selvtest/ProfileAct.java
@@ -1,8 +1,12 @@
 package com.example.quiz_selvtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ProfileAct extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class ProfileAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profile_topbar);
+        TextView title = (TextView)findViewById(R.id.profile_toolbar_text);
+        title.setText("Profile");
+        findViewById(R.id.profile_toolbar_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    public void settings(View view) {
+        Intent intent = new Intent(this, SettingsAct.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/quiz_selvtest/SettingsAct.java
+++ app/src/main/java/com/example/quiz_selvtest/SettingsAct.java
@@ -1,8 +1,13 @@
 package com.example.quiz_selvtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SettingsAct extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class SettingsAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.settings_topbar);
+        TextView title = (TextView)findViewById(R.id.settings_toolbar_text);
+        title.setText("Settings");
+        findViewById(R.id.settings_toolbar_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
