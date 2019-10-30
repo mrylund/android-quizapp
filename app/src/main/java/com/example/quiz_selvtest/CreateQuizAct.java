2265c079a402b7a2035b7791e6539296070738ea
Rasmus <rasmus-strange@hotmail.com>
2019-10-30 14:47:13 +0100

    Første par aktiviteter uden indhold

---
+++ app/src/main/java/com/example/quiz_selvtest/CreateQuizAct.java
@@ -0,0 +1,14 @@
+package com.example.quiz_selvtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateQuizAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_quiz);
+    }
+}
---
+++ app/src/main/java/com/example/quiz_selvtest/NoAccountAct.java
@@ -0,0 +1,14 @@
+package com.example.quiz_selvtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoAccountAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_account);
+    }
+}
