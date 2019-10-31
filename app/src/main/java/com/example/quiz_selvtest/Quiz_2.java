259f223ae5292c8b07eb25ff3cbb30090ddfef2c
= <martin@rylund.eu>
2019-10-31 16:16:29 +0100

    Tilføjet quizzen efter at man indtaster kode

--- app/src/main/java/com/example/quiz_selvtest/Quiz_2.java
+++ app/src/main/java/com/example/quiz_selvtest/Quiz_2.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class NoAccountAct extends AppCompatActivity {
+public class Quiz_2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_no_account);
+        setContentView(R.layout.quiz_2);
     }
 
-    public void joinQuiz(View view) {
-        Intent intent = new Intent(this, Quiz_1.class);
+
+    public void checkAnswer(View view) {
+        Intent intent = new Intent(this, Quiz_done.class);
         startActivity(intent);
     }
 }
