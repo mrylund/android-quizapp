48a1f91c862e0881c7daad98554117a19928d2c9
Spyingaround <athu57@hotmail.dk>
2019-10-31 17:10:02 +0100

    Merged and added gobackbtn

--- app/src/main/java/com/example/quiz_selvtest/NonStudentSigninAct.java
+++ app/src/main/java/com/example/quiz_selvtest/NonStudentSigninAct.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class NonStudentSigninAct extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class NonStudentSigninAct extends AppCompatActivity {
         setContentView(R.layout.activity_non_student_signin);
 
         ImageView goBackBTN = findViewById(R.id.goBackBTN2);
+        Button signIn = findViewById(R.id.button2);
 
         goBackBTN.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +24,15 @@ public class NonStudentSigninAct extends AppCompatActivity {
                 startActivity(new Intent(NonStudentSigninAct.this, StartScreenAct.class));
             }
         });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NonStudentSigninAct.this, FragmentController.class));
+            }
+        });
+
+
 
     }
 }
