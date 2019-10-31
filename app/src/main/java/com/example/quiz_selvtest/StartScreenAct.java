101b46a74e4b5d3506d547444bb1cf5ca72198e4
= <martin@rylund.eu>
2019-10-31 15:36:54 +0100

    Tilføjet No account join quiz activity

--- app/src/main/java/com/example/quiz_selvtest/StartScreenAct.java
+++ app/src/main/java/com/example/quiz_selvtest/StartScreenAct.java
@@ -2,7 +2,9 @@ package com.example.quiz_selvtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartScreenAct extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class StartScreenAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startscreen);
+    }
+
+
+    public void playWithoutAccount(View view) {
+        Intent intent = new Intent(this, NoAccountAct.class);
+        startActivity(intent);
     }
 }
