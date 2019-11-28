9ad94f0aa434b4183fc199962e1dc5e9f24bc998
= <martin@rylund.eu>
2019-11-28 21:47:38 +0100

    Added logic of Quiz activity

--- app/src/main/java/com/example/quiz_selvtest/Quiz.java
+++ app/src/main/java/com/example/quiz_selvtest/Quiz.java
@@ -2,30 +2,107 @@ package com.example.quiz_selvtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.quiz_selvtest.quizLogic.QuizHandler;
+
+import java.io.IOException;
 
 public class Quiz extends AppCompatActivity {
+    private QuizHandler game;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        intent = getIntent();
+        new StartQuiz().execute("");
     }
 
-    private void StartQuiz(String quizCode) {
+    private class StartQuiz extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String status;
+            getIntent();
+            String quizCode = intent.getStringExtra("quizCode");
+            try {
+                game = new QuizHandler(quizCode);
+                status = "Success";
+                System.out.println(game.getQuestion());
+            } catch (IOException e) {
+                e.printStackTrace();
+                status = "Fail";
+            }
+            return status;
+        }
 
+        @Override
+        protected void onPostExecute(String s) {
+            if (s.equals("Success")) {
+                setInfo();
+            } else {
+                // If no internet connection the app will fail.
+                setQuestion("Something went wrong");
+            }
+        }
+    }
+
+    private void setInfo() {
+        String q = game.getQuestion();
+        String[] ans = game.getAnswers();
+
+        int n = 0;
+        for (String a : ans) {
+            n++;
+            setAnswer(n, a);
+        }
+
+        setQuestion(q);
+    }
+
+    private void pressAnswer(int n) {
+        if (game.checkAnswer(n, false)) {
+            game.addScore();
+        }
+        game.nextQuestion();
+        setInfo();
+        
+        if (game.hasEnded()) {
+            endGame();
+        }
+    }
+
+    private void endGame() {
+        Intent intent = new Intent(this, Quiz_done.class);
+        intent.putExtra("game", game);
+        startActivity(intent);
     }
 
 
-    private void setAnswer1(String a) {
+    private void setQuestion(String a) {
+        TextView q = findViewById((R.id.Quiz_Question));
+        q.setText(a);
+    }
+
+    private void setAnswer(final int n, String a) {
         Button btn = findViewById(R.id.Quiz_btn1);
+        switch(n) {
+            case 1: btn = findViewById(R.id.Quiz_btn1); break;
+            case 2: btn = findViewById(R.id.Quiz_btn2); break;
+            case 3: btn = findViewById(R.id.Quiz_btn3); break;
+            case 4: btn = findViewById(R.id.Quiz_btn4); break;
+        }
         btn.setText(a);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                pressAnswer(n);
             }
         });
     }
