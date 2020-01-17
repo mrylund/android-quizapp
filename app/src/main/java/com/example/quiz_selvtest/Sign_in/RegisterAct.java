8de5151a7d9290743cc7cb9430dc5fd4f01323a8
Spyingaround <athu57@hotmail.dk>
2020-01-17 03:13:19 +0100

    Begyndelse på kode-skrivning i "Registrer" aktiviteten, hvor de forskellige "EditText" skal valideres.

--- app/src/main/java/com/example/quiz_selvtest/Sign_in/RegisterAct.java
+++ app/src/main/java/com/example/quiz_selvtest/Sign_in/RegisterAct.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.quiz_selvtest.Activity.StartScreenAct;
@@ -13,11 +16,16 @@ import com.example.quiz_selvtest.R;
 
 public class RegisterAct extends AppCompatActivity {
 
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    private boolean allAccepted = false;
+    private boolean emailAccepted = false;
+    private boolean usernameAccepted = false;
+    private boolean passwordAccepted = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
         ImageView goBack = findViewById(R.id.goBackBTN);
 
         goBack.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +34,41 @@ public class RegisterAct extends AppCompatActivity {
                 finish();
             }
         });
+
+    }
+
+    private void emailAccepted(){
+        final EditText emailToValidate = findViewById(R.id.registerMail);
+        final String email = emailToValidate.getText().toString().trim();
+
+        emailToValidate.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(email.matches(emailPattern) && s.length() > 0){
+                    emailAccepted = true;
+                }
+            }
+        });
+    }
+
+    private void isPasswordAccepted(){
+        final EditText passWordToValidate = findViewById(R.id.registerPassword);
+        final EditText repeatPassWord = findViewById(R.id.repeatPassword);
+
+        String pw = passWordToValidate.getText().toString().trim();
+        String repeatPW = repeatPassWord.getText().toString().trim();
+
+
 
     }
 }
