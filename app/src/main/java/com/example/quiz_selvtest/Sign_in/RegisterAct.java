c3aa180ed702edb81830c609f67df0239ffccab8
Spyingaround <athu57@hotmail.dk>
2020-01-19 00:12:33 +0100

    I RegisterAct er der blevet lavet en metode til pop-up dialogen, så koden er nemmere at læse og forstå. Der er også blevet fixet en error i if-statement, som gjorde at den ikke tjekkede password og email-pattern korrekt. Derudover er der blevet tilføjet en rundt knap til pop-up, samt margins i pop-up dialogen.

--- app/src/main/java/com/example/quiz_selvtest/Sign_in/RegisterAct.java
+++ app/src/main/java/com/example/quiz_selvtest/Sign_in/RegisterAct.java
@@ -17,12 +17,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.quiz_selvtest.Activity.StartScreenAct;
+import com.example.quiz_selvtest.Fragment.FragmentController;
 import com.example.quiz_selvtest.R;
 
 public class RegisterAct extends AppCompatActivity {
 
     private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     final Context context = this;
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +37,6 @@ public class RegisterAct extends AppCompatActivity {
         final EditText passwordToConfirm = findViewById(R.id.repeatPassword);
         ImageView goBack = findViewById(R.id.goBackBTN);
         final Button register = findViewById(R.id.registerButton);
-
 
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,40 +52,38 @@ public class RegisterAct extends AppCompatActivity {
                 String userName = userNameToRegister.getText().toString().trim();
                 String password = passwordToRegister.getText().toString().trim();
                 String passwordConfirm = passwordToConfirm.getText().toString().trim();
+
                 // TODO: 1/17/2020  Database kald til at tjekke om userName og email allerede eksistere i databasen
-
-                if(!email.equals(emailPattern) || email.isEmpty() || userName.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty() ){
-
-                    final Dialog dialog = new Dialog(context);
-                    dialog.setContentView(R.layout.custom_dialog);
-
-                    TextView dialogIMG = dialog.findViewById(R.id.dialogTitle);
-                    dialogIMG.setText("UH-OH! Missing field");
-
-                    TextView dialogTXT = dialog.findViewById(R.id.dialogText);
-                    dialogTXT.setText("Check for errors in field");
-
-                    Button dialogButton = dialog.findViewById(R.id.dialogButtonOK);
-                    dialogButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            dialog.dismiss();
-                        }
-                    });
-
-                    dialog.show();
-
+                if(!email.matches(emailPattern) || email.isEmpty() || userName.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty() ){
+                    initCustomDialog("UH-OH! Error in field!","Please verify your inputs, and try again!");
+                    return;
                 }
 
-                if(!password.equals(passwordConfirm)){
-
+                if(!password.matches(passwordConfirm)){
+                    initCustomDialog("UH-OH! Password differs!","The passwords you have typed in, do not match each other");
                 }else{
-
+                    startActivity(new Intent(RegisterAct.this, FragmentController.class));
                 }
-
 
             }
         });
+    }
+
+    public void initCustomDialog(String title, String description){
+        dialog = new Dialog(context);
+        dialog.setContentView(R.layout.custom_dialog);
+        TextView dialogTitle = dialog.findViewById(R.id.dialogTitle);
+        dialogTitle.setText(title);
+        TextView dialogDesc = dialog.findViewById(R.id.dialogText);
+        dialogDesc.setText(description);
+        Button dialogButton = dialog.findViewById(R.id.dialogButtonOK);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 
 }
