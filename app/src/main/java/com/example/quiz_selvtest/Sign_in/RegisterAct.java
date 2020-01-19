7922811f900f9220890ef181c75e5f1008dfda62
= <martin@rylund.eu>
2020-01-19 02:00:46 +0100

    Tilføjet logik til registrering af bruger, nu bliver brugere tilføjet til firebase

--- app/src/main/java/com/example/quiz_selvtest/Sign_in/RegisterAct.java
+++ app/src/main/java/com/example/quiz_selvtest/Sign_in/RegisterAct.java
@@ -1,5 +1,6 @@
 package com.example.quiz_selvtest.Sign_in;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -10,26 +11,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.quiz_selvtest.Activity.StartScreenAct;
 import com.example.quiz_selvtest.Fragment.FragmentController;
 import com.example.quiz_selvtest.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterAct extends AppCompatActivity {
 
     private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     final Context context = this;
     private Dialog dialog;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "RegisterAct";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         final EditText emailToValidate = findViewById(R.id.registerMail);
         final EditText userNameToRegister = findViewById(R.id.registerUsername);
@@ -62,11 +75,37 @@ public class RegisterAct extends AppCompatActivity {
                 if(!password.matches(passwordConfirm)){
                     initCustomDialog("UH-OH! Password differs!","The passwords you have typed in, do not match each other");
                 }else{
-                    startActivity(new Intent(RegisterAct.this, FragmentController.class));
+
+                    createUser(email, password);
+                    //startActivity(new Intent(RegisterAct.this, FragmentController.class));
                 }
 
             }
         });
+    }
+
+    public void createUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            doUserRegistered(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterAct.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void doUserRegistered(FirebaseUser user) {
+        //TODO add stuff here :)
     }
 
     public void initCustomDialog(String title, String description){
