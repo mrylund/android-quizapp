eae7d254205a57e1de096759cd5be7e28465bfcc
Athusan <athusan_kuga@hotmail.com>
2020-01-22 12:38:16 +0100

    Tilføjet elemter til course-fragment.

--- app/src/main/java/com/example/quiz_selvtest/Fragment/My_coursesFrag.java
+++ app/src/main/java/com/example/quiz_selvtest/Fragment/My_coursesFrag.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.quiz_selvtest.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +25,17 @@ public class My_coursesFrag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.homefragment,container,false);
+
+        EditText courseID = v.findViewById(R.id.courseIDInput);
+        Button joinCourseBTN = v.findViewById(R.id.joinCourseBTN);
+        ListView myCoursesListView = v.findViewById(R.id.myCoursesList);
+        ArrayList<Course> courseList = new ArrayList<>();
         return inflater.inflate(R.layout.my_courses,container,false);
+
+
+
+
     }
 }
