43b1d9f66018154b029afe2f37bfa6f0b0cbf323
Martin Rylund <martin@rylund.eu>
2020-01-22 18:01:08 +0100

    Ryddet op i register user info

--- app/src/main/java/com/example/quiz_selvtest/Sign_in/RegisterUserInfo.java
+++ app/src/main/java/com/example/quiz_selvtest/Sign_in/RegisterUserInfo.java
@@ -5,21 +5,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.quiz_selvtest.Activity.StartScreenAct;
 import com.example.quiz_selvtest.Fragment.FragmentController;
-import com.example.quiz_selvtest.Quiz.CreateQuizAct;
 import com.example.quiz_selvtest.R;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -36,20 +29,24 @@ public class RegisterUserInfo extends AppCompatActivity {
 
 
     public void registerInfo(View v) {
-        EditText usernameET = findViewById(R.id.registerUsername);
-        EditText languageET = findViewById(R.id.registerLanguage);
+        EditText usernameET = findViewById(R.id.txtUsername);
+        EditText languageET = findViewById(R.id.txtLanguage);
         String username = usernameET.getText().toString();
         String language = languageET.getText().toString();
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+        // Make a map containing the info the user entered
         Map<String, Object> userInfo = new HashMap<>();
         userInfo.put("Username", username);
         userInfo.put("Language", language);
 
+        // Get the current user
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
+        // Add the userdata to the database, then redirect them to the home
+        assert currentUser != null;
         db.collection("Users").document(currentUser.getUid())
                 .set(userInfo).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
