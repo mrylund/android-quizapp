736d823c21e96f3c46ce04a2236caa1311c8334e
Martin Rylund <martin@rylund.eu>
2020-01-22 14:44:15 +0100

    Ryddet op i start skærm

--- app/src/main/java/com/example/quiz_selvtest/Activity/StartScreenAct.java
+++ app/src/main/java/com/example/quiz_selvtest/Activity/StartScreenAct.java
@@ -14,14 +14,11 @@ import com.example.quiz_selvtest.Sign_in.ForgotPWAct;
 import com.example.quiz_selvtest.Sign_in.NoAccountAct;
 import com.example.quiz_selvtest.Sign_in.NonStudentSigninAct;
 import com.example.quiz_selvtest.Sign_in.RegisterAct;
-import com.example.quiz_selvtest.Sign_in.RegisterUserInfo;
 import com.example.quiz_selvtest.Sign_in.StudentSigninAct;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -33,42 +30,40 @@ public class StartScreenAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //URL reference to our database. Everything will be stored in the link below.
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReferenceFromUrl("https://quizr-c3c3b.firebaseio.com/");
-
-        // Initialize Firebase Auth
+        // Initialize Firebase Authentication
         mAuth = FirebaseAuth.getInstance();
 
 
         setContentView(R.layout.activity_startscreen);
 
-        Button button = findViewById(R.id.startscreenbutton2);
-        Button button2 = findViewById(R.id.button);
-        TextView register = findViewById(R.id.registerTxt);
-        TextView forgotpw = findViewById(R.id.forgotPWTextView);
+        // Find objects and save them in variables, then define the onClick methods
+        Button btnNonStudent = findViewById(R.id.nonStudentBtn);
+        Button btnStudentTeacher = findViewById(R.id.studentTeacherBtn);
+        TextView txtRegister = findViewById(R.id.registerTxt);
+        TextView txtForgotPW = findViewById(R.id.forgotPWTextView);
 
-        button2.setOnClickListener(new View.OnClickListener() {
+
+        btnStudentTeacher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(StartScreenAct.this, StudentSigninAct.class));
             }
         });
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btnNonStudent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(StartScreenAct.this, NonStudentSigninAct.class));
             }
         });
 
-        register.setOnClickListener(new View.OnClickListener() {
+        txtRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(StartScreenAct.this, RegisterAct.class));
             }
         });
-        forgotpw.setOnClickListener(new View.OnClickListener() {
+        txtForgotPW.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(StartScreenAct.this, ForgotPWAct.class));
@@ -80,13 +75,14 @@ public class StartScreenAct extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
+        // Check if  a user is signed in (user object Not-null)
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
             DocumentReference userInfo = FirebaseFirestore.getInstance().collection("Users").document(currentUser.getUid());
             userInfo.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                 @Override
                 public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    // If a user is signed in already, then redirect the user to the home screen
                     if (task.isSuccessful()) {
                         DocumentSnapshot document = task.getResult();
                         if (document != null && document.exists()) {
