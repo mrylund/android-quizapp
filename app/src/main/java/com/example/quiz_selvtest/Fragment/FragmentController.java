d7b3b31493264b6d3b64c8871a359c345fb92ca7
Martin Rylund <martin@rylund.eu>
2020-01-22 19:58:13 +0100

    Ny fragment controller, ny bruges der også viewpager så man kan swipe mellem fragmenter

--- app/src/main/java/com/example/quiz_selvtest/Fragment/FragmentController.java
+++ app/src/main/java/com/example/quiz_selvtest/Fragment/FragmentController.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.Intent;
@@ -15,29 +16,33 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.quiz_selvtest.Fragment.my_quizzes.MyQuiz;
 import com.example.quiz_selvtest.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+
+// Inspired by: https://droidmentor.com/bottomnavigationview-with-viewpager-android/
 public class FragmentController extends AppCompatActivity {
     int backButtonCount = 0;
     Context context = this;
     BottomNavigationView bottomnav;
-    static int[][] states = new int[][] {
-            new int[] {android.R.attr.state_checked},//checked
-            new int[] {-android.R.attr.state_checked} // unchecked
-    };
+    ProfileFragment profileFragment;
+    HomeFragment homeFragment;
+    My_coursesFrag coursesFragment;
+    ViewPager viewPager;
+    MenuItem prevMenuItem;
 
-    static int[] colors = new int[] {
-            Color.BLACK,
-            Color.GRAY,
-    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
         backButtonCount = 0;
 
-        // Change color on all the icons in the bottom navigation menu
+        bottomnav = findViewById(R.id.bottom_navigation);
+        bottomnav.setOnNavigationItemSelectedListener(navlistner);
+
+        /*// Change color on all the icons in the bottom navigation menu
         changeIconTint(R.drawable.profile,R.color.darkGrey);
         changeIconTint(R.drawable.books,R.color.darkGrey);
         changeIconTint(R.drawable.homelogo,R.color.darkGrey);
@@ -51,24 +56,47 @@ public class FragmentController extends AppCompatActivity {
         bottomnav.setItemTextColor(colorList);
 
         // Set navigation listener and set the fragment
-        bottomnav.setOnNavigationItemSelectedListener(navlistner);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+        bottomnav.setOnNavigationItemSelectedListener(navlistner);*/
+        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
+        viewPager = findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null)
+                    prevMenuItem.setChecked(false);
+                else
+                    bottomnav.getMenu().getItem(1).setChecked(false);
+
+                bottomnav.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomnav.getMenu().getItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        viewPager.setCurrentItem(1);
     }
 
     // Inspired by: https://stackoverflow.com/a/16383385
     @Override
     public void onBackPressed()
     {
-        //TODO: Make it properly select the menu items - currently it doesn't change selected state.
-
-        // Update the fragment stack, incase something has not been pushed yet
-        getSupportFragmentManager().executePendingTransactions();
-
-        // Pop the backstack
-        if (getSupportFragmentManager().getBackStackEntryCount() >= 1) {
-            getSupportFragmentManager().popBackStackImmediate();
+        if (viewPager.getCurrentItem() != 1) {
+            viewPager.setCurrentItem(1);
             return;
         }
+
 
         // If you press the back button when nothing is in the backstack, notify the user if they press
         // back again it will close the app.
@@ -90,40 +118,33 @@ public class FragmentController extends AppCompatActivity {
     private BottomNavigationView.OnNavigationItemSelectedListener navlistner = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            String name = "1";
-            Fragment selectedF = new HomeFragment();
             backButtonCount = 0;
 
             // Switch on the menu item, then set the data for the item
             switch (menuItem.getItemId()) {
                 case R.id.Home:
-                    selectedF = new HomeFragment();
-                    name = "1";
+                    viewPager.setCurrentItem(1);
                     break;
                 case R.id.courses:
-                    selectedF = new My_coursesFrag();
-                    name = "2";
+                    viewPager.setCurrentItem(2);
                     break;
                 case R.id.your_profile:
-                    selectedF = new ProfileFragment();
-                    name = "3";
+                    viewPager.setCurrentItem(0);
                     break;
             }
-
-            // Mulighed for at pop backstack for at undgå uendelig mange af de samme fragmenter i stack.
-            // Fejl med at ullige antal i backstack ikke tracker.
-            //getSupportFragmentManager().popBackStack(name, 1);
-
-            // Opdater fragment
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedF).addToBackStack(name).commit();
-            return true;
+            return false;
         }
     };
 
-    // Change the color of the icon
-    public void changeIconTint( int iconNum, int colorRes){
-        Drawable icon = ContextCompat.getDrawable(context,iconNum);
-        assert icon != null;
-        icon.setColorFilter(ContextCompat.getColor(context,colorRes), PorterDuff.Mode.SRC_IN);
+    private void setupViewPager(ViewPager viewPager)
+    {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        profileFragment =new ProfileFragment();
+        homeFragment =new HomeFragment();
+        coursesFragment =new My_coursesFrag();
+        adapter.addFragment(profileFragment);
+        adapter.addFragment(homeFragment);
+        adapter.addFragment(coursesFragment);
+        viewPager.setAdapter(adapter);
     }
 }
